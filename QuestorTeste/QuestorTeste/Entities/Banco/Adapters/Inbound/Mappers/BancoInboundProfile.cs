b9aba76e1d716f80851d00ad6f7ac4763f05a1fa
Bruno Mokan Muenchen <brunomokan@gmail.com>
2026-06-13 03:51:27 -0300

    feat: adds Banco mappers

---
+++ QuestorTeste/QuestorTeste/Entities/Banco/Adapters/Inbound/Mappers/BancoInboundProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using QuestorTeste.Entities.Banco.Adapters.Inbound.Dtos;
+using QuestorTeste.Entities.Banco.Domain.Models;
+
+namespace QuestorTeste.Entities.Banco.Adapters.Inbound.Mappers;
+
+public class BancoInboundProfile: Profile
+{
+    public BancoInboundProfile()
+    {
+        CreateMap<CriarBancoRequestDto, BancoModel>()
+            .ForMember(dest => dest.Id, opt => opt.Ignore()); 
+    }
+}
---
+++ QuestorTeste/QuestorTeste/Entities/Banco/Adapters/Outbound/Mappers/BancoOutboundProfile.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using QuestorTeste.Entities.Banco.Adapters.Outbound.Entities;
+using QuestorTeste.Entities.Banco.Domain.Models;
+
+namespace QuestorTeste.Entities.Banco.Adapters.Outbound.Mappers;
+
+public class BancoOutboundProfile : Profile
+{
+    public BancoOutboundProfile()
+    {
+        CreateMap<BancoModel, BancoEntity>().ReverseMap();
+    }
+}

232691373c69cba1758a8a0565bf9f657df7f0a4
Bruno Mokan Muenchen <brunomokan@gmail.com>
2026-06-13 03:58:28 -0300

    feat: adds Boleto mappers

---
+++ QuestorTeste/QuestorTeste/Entities/Boleto/Adapters/Inbound/Mappers/BoletoInboundProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using QuestorTeste.Entities.Boleto.Adapters.Inbound.Dtos;
+using QuestorTeste.Entities.Boleto.Domain.Models;
+
+namespace QuestorTeste.Entities.Boleto.Adapters.Inbound.Mappers;
+
+public class BoletoInboundProfile : Profile
+{
+    public BoletoInboundProfile()
+    {
+        CreateMap<CriarBoletoRequestDto, BoletoModel>()
+            .ForMember(dest => dest.Id, opt => opt.Ignore());
+    }
+}
---
+++ QuestorTeste/QuestorTeste/Entities/Boleto/Adapters/Outbound/Mappers/BoletoOutboundProfile.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using QuestorTeste.Entities.Boleto.Adapters.Outbound.Entities;
+using QuestorTeste.Entities.Boleto.Domain.Models;
+
+namespace QuestorTeste.Entities.Boleto.Adapters.Outbound.Mappers;
+
+public class BoletoOutboundProfile : Profile
+{
+    public BoletoOutboundProfile()
+    {
+        CreateMap<BoletoModel, BoletoEntity>().ReverseMap();
+    }
+}
